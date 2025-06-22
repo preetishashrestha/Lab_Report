1e2385d06d93882d86d50f80da195581f7c2a88c
preetishashrestha <preetishashrestha@gmail.com>
2025-06-22 14:08:46 +0545

    lab 1 que number 1

---
+++ Lab_Report/lab_1_que_1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Report
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter your name: ");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine($"Welcome {Name}, to dotnet technology");
+        }
+    }
+}
